9ef128017488b442fbd3ca238d8e69cbd14665f7
Het Patel <hetpatel@svitvasad.ac.in>
2025-10-05 23:27:59 +0530

    added some minor details

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function greet() {
   alert("Hello! You just clicked the button!");
+  console.log("Button clicked!");
+  document.body.style.backgroundColor = "lightblue";
+  document.body.style.color = "white";
+  document.body.style.fontFamily = "Arial, sans-serif";
+  document.body.style.padding = "20px";
+  document.body.style.textAlign = "center";
+  document.body.style.transition = "background-color 0.5s ease";
+  document.body.innerHTML += "<p>Enjoy the new look!</p>";
 }
